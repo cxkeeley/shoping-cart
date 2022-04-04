388604d9137e8d5bc24cf173c66ba3ab5a0cdb2f
cxkeeley <cxkeeley@gmail.com>
2022-04-04 20:30:24 +0700

    Initial commit

---
+++ src/components/ProductList.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <h1>Product List</h1>
+    <img
+      v-if="loading"
+      src="https://i.imgur.com/JfPpwOA.gif"
+      alt="loading"
+      width="50"
+      height="50"
+    >
+    <ul v-else>
+      <li v-for="product in products">
+        {{ product.title }} - {{ product.price | currency }} - {{ product.inventory }}
+        <button
+          :disabled="!productIsInStock(product)"
+          @click="addProductToCart(product)"
+        >Add to cart</button>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        loading: false
+      }
+    },
+
+    computed: {
+      products () {
+        return this.$store.state.products
+      },
+
+      productIsInStock () {
+        return this.$store.getters.productIsInStock
+      }
+    },
+
+    methods: {
+      addProductToCart (product) {
+        this.$store.dispatch('addProductToCart', product)
+      }
+    },
+
+    created () {
+      this.loading = true
+      this.$store.dispatch('fetchProducts')
+        .then(() => this.loading = false)
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
